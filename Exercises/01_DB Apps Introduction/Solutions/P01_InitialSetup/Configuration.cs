bebe2f7d1e2b3e9d2dd44a4da57036c0a229b088
Borislav Valkov <vulkov.borislav@gmal.cm>
2019-10-04 17:10:13 +0300

    Add P01_InitialSetup

---
+++ Exercises/01_DB Apps Introduction/Solutions/P01_InitialSetup/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace P01_InitialSetup
+{
+    public static class Configuration
+    {
+        public const string CoonnectionString = @"Server=DESKTOP-I00IPSS;;Integrated Security=True";
+    }
+}
