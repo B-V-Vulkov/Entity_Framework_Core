e64d90b84f840e9a94041bf9a20a15f332c707f3
Borislav Valkov <vulkov.borislav@gmal.cm>
2019-10-05 15:21:03 +0300

    Add solutions

---
+++ Exercises/01_DB Apps Introduction/Solutions/P04.AddMinion/StartUp.cs
@@ -0,0 +1,39 @@
+﻿namespace P04.AddMinion
+{
+    using System;
+    using System.Data.SqlClient;
+
+    class StartUp
+    {
+        static void Main()
+        {
+            string[] minionInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string[] villianInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string minionName = minionInfo[1];
+            int minionAge = int.Parse(minionInfo[2]);
+            string minionTown = minionInfo[3];
+            string villianName = villianInfo[1];
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = Configuration.CoonnectionString;
+                connection.Open();
+
+                string villianIdQuery = "SELECT Id FROM Villains WHERE Name = @Name";
+
+                using (SqlCommand command = new SqlCommand(villianIdQuery, connection))
+                {
+                    command.Parameters.AddWithValue("@Name", villianName);
+                    int villianId = (int)command.ExecuteScalar();
+
+
+                    Console.WriteLine(villianId);
+                }
+            }
+        }
+    }
+}
