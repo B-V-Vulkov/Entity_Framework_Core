9b7e3ff56443164c03e33f7cde229a369fcc85a7
Borislav Valkov <vulkov.borislav@gmal.cm>
2019-10-05 14:23:56 +0300

    Add P02.VillainNames

---
+++ Exercises/01_DB Apps Introduction/Solutions/P02.VillainNames/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace P02.VillainNames
+{
+    public static class Configuration
+    {
+        public const string CoonnectionString = @"Server=DESKTOP-I00IPSS;DataBase=MinionsDB;Integrated Security=True";
+    }
+}
