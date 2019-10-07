29cadc06086544bde5ab138a2ff0c004485adaa6
Borislav Valkov <vulkov.borislav@gmal.com>
2019-10-07 12:26:11 +0300

    Add P05.ChangeTownNamesCasing
    Co-Authored-By: Borislav Valkov <b-v-vulkov@users.noreply.github.com>

---
+++ Exercises/01_DB Apps Introduction/Solutions/P05.ChangeTownNamesCasing/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace P05.ChangeTownNamesCasing
+{
+    public static class Configuration
+    {
+        public const string CoonnectionString = @"Server=DESKTOP-I00IPSS;DataBase=MinionsDB;Integrated Security=True";
+    }
+}
---
+++ Exercises/01_DB Apps Introduction/Solutions/P05.ChangeTownNamesCasing/StartUp.cs
@@ -0,0 +1,66 @@
+﻿namespace P05.ChangeTownNamesCasing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            string country = Console.ReadLine();
+
+            using (SqlConnection connection = new SqlConnection())
+            {
+                connection.ConnectionString = Configuration.CoonnectionString;
+                connection.Open();
+
+                string updateQuery = @"UPDATE Towns
+                                        SET Name = UPPER(Name)
+                                        WHERE CountryCode = (SELECT c.Id FROM Countries AS c WHERE c.Name = @countryName)";
+
+                using (SqlCommand updateCommand = new SqlCommand(updateQuery, connection))
+                {
+                    updateCommand.Parameters.AddWithValue("@countryName", country);
+                    int numberOfrows = updateCommand.ExecuteNonQuery();
+
+                    if (numberOfrows == 0)
+                    {
+                        Console.WriteLine("No town names were affected.");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{numberOfrows} town names were affected.");
+                        List<string> townNames = GetTownName(country, connection);
+                        Console.WriteLine("[" + string.Join(", ", townNames) + "]");
+                    }
+                }
+            }
+        }
+
+        private static List<string> GetTownName(string country, SqlConnection connection)
+        {
+            string selectQuery = @"SELECT t.Name 
+                                        FROM Towns as t
+                                        JOIN Countries AS c ON c.Id = t.CountryCode
+                                        WHERE c.Name = @countryName";
+
+            using (SqlCommand selectCommand = new SqlCommand(selectQuery, connection))
+            {
+                selectCommand.Parameters.AddWithValue("@countryName", country);
+
+                List<string> townNames = new List<string>();
+
+                using (SqlDataReader reader = selectCommand.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        townNames.Add((string)reader[0]);
+                    }
+
+                    return townNames;
+                }
+            }
+        }
+    }
+}
