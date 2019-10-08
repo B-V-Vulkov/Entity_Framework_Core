b28b44f04f9ed0b737a7bc8305a65539912935de
Borislav Valkov <vulkov.borislav@gmal.com>
2019-10-08 18:48:07 +0300

    Add P08.AddressesByTown
    Co-Authored-By: Borislav Valkov <b-v-vulkov@users.noreply.github.com>

---
+++ Exercises/03_Entity Framework Introduction/Solutions/P08.AddressesByTown/StartUp.cs
@@ -0,0 +1,46 @@
+﻿namespace SoftUni
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    using Data;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var context = new SoftUniContext())
+            {
+                var result = GetAddressesByTown(context);
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetAddressesByTown(SoftUniContext context)
+        {
+            var addresses = context.Addresses
+                .Select(a => new
+                {
+                    a.AddressText,
+                    TownName = a.Town.Name,
+                    EmployeeCount = a.Employees.Count(),
+                })
+                .OrderByDescending(c => c.EmployeeCount)
+                .ThenBy(t => t.TownName)
+                .ThenBy(a => a.AddressText)
+                .Take(10);
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            foreach (var address in addresses)
+            {
+                stringBuilder.AppendLine($"{address.AddressText}, " +
+                    $"{address.TownName} - " +
+                    $"{address.EmployeeCount} employees");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
