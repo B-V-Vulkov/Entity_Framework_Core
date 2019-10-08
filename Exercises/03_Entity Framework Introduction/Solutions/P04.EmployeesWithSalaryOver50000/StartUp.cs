4dd3ad5460d51a68b3bf1f1521b2943880a6309d
Borislav Valkov <vulkov.borislav@gmal.com>
2019-10-08 15:06:22 +0300

    Add P04.EmployeesWithSalaryOver50000
    Co-Authored-By: Borislav Valkov <b-v-vulkov@users.noreply.github.com>

---
+++ Exercises/03_Entity Framework Introduction/Solutions/P04.EmployeesWithSalaryOver50000/StartUp.cs
@@ -0,0 +1,41 @@
+﻿namespace SoftUni
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    using Data;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var context = new SoftUniContext())
+            {
+                var result = GetEmployeesWithSalaryOver50000(context);
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetEmployeesWithSalaryOver50000(SoftUniContext context) 
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.Salary
+                })
+                .Where(e => e.Salary > 50000)
+                .OrderBy(e => e.FirstName);
+
+            foreach (var employee in employees)
+            {
+                stringBuilder.AppendLine($"{employee.FirstName} - {employee.Salary:f2}");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
