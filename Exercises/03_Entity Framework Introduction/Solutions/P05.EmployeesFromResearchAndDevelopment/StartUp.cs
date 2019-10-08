c630e17272bfcb3de073302ada5df05369e631e2
Borislav Valkov <vulkov.borislav@gmal.com>
2019-10-08 15:43:03 +0300

    Add P05.EmployeesFromResearchAndDevelopment
    Co-Authored-By: Borislav Valkov <b-v-vulkov@users.noreply.github.com>

---
+++ Exercises/03_Entity Framework Introduction/Solutions/P05.EmployeesFromResearchAndDevelopment/StartUp.cs
@@ -0,0 +1,47 @@
+﻿namespace SoftUni
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    using Data;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var context = new SoftUniContext())
+            {
+                var result = GetEmployeesFromResearchAndDevelopment(context);
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context) 
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            var employees = context.Employees
+                .Where(d => d.Department.Name == "Research and Development")
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    DepartmentName = e.Department.Name,
+                    e.Salary
+                })
+                .OrderBy(e => e.Salary)
+                .ThenByDescending(e => e.FirstName);
+
+            foreach (var employee in employees)
+            {
+                stringBuilder.AppendLine($"{employee.FirstName} " +
+                    $"{employee.LastName} from " +
+                    $"{employee.DepartmentName} - " +
+                    $"${employee.Salary:f2}");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
