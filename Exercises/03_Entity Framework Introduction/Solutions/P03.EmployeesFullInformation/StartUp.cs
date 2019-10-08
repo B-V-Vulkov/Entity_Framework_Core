f28722e5ed5efc4121108dba85fedb102a6c1e0b
Borislav Valkov <vulkov.borislav@gmal.com>
2019-10-08 14:54:41 +0300

    Add P03.EmployeesFullInformation
    Co-Authored-By: Borislav Valkov <b-v-vulkov@users.noreply.github.com>

---
+++ Exercises/03_Entity Framework Introduction/Solutions/P03.EmployeesFullInformation/StartUp.cs
@@ -0,0 +1,48 @@
+﻿namespace SoftUni
+{
+    using Data;
+    using System;
+    using System.Text;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var context = new SoftUniContext())
+            {
+                var resuult = GetEmployeesFullInformation(context);
+                Console.WriteLine(resuult);
+            }
+        }
+
+        public static string GetEmployeesFullInformation(SoftUniContext context)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            var employees = context.Employees
+                .Select(e => new
+                {
+                    e.FirstName,
+                    e.LastName,
+                    e.MiddleName,
+                    e.JobTitle,
+                    e.Salary,
+                    e.EmployeeId
+                })
+                .OrderBy(x => x.EmployeeId);
+
+            foreach (var employee in employees)
+            {
+                stringBuilder.AppendLine(
+                    $"{employee.FirstName} " +
+                    $"{employee.LastName} " +
+                    $"{employee.MiddleName} " +
+                    $"{employee.JobTitle} " +
+                    $"{employee.Salary:f2}");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
