a630be7719444a589c669600c719c6cd769e42d1
Borislav Valkov <vulkov.borislav@gmal.com>
2019-10-08 19:17:48 +0300

    Add solution P09

---
+++ Exercises/03_Entity Framework Introduction/Solutions/P09.Employee147/StartUp.cs
@@ -0,0 +1,52 @@
+﻿namespace SoftUni
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    using Data;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var context = new SoftUniContext())
+            {
+                var result = GetEmployee147(context);
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string GetEmployee147(SoftUniContext context) 
+        {
+            var employee147 = context.Employees
+                .Select(e => new 
+                {
+                    e.EmployeeId,
+                    e.FirstName,
+                    e.LastName,
+                    e.JobTitle,
+                    projects = e.EmployeesProjects
+                    .Select(p => new 
+                    {
+                        p.Project.Name
+                    })
+                    .OrderBy(p => p.Name)
+                })
+                .FirstOrDefault(id => id.EmployeeId == 147);
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            stringBuilder.AppendLine($"{employee147.FirstName} " +
+                $"{employee147.LastName} - " +
+                $"{employee147.JobTitle}");
+
+            foreach (var project in employee147.projects)
+            {
+                stringBuilder.AppendLine($"{project.Name}");
+            }
+
+            return stringBuilder.ToString().TrimEnd();
+        }
+    }
+}
