4d49603c67d156c41059b081f9a3c2b602a6de9c
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-10-09 13:48:30 +0300

    Add P15.RemoveTown

---
+++ Exercises/03_Entity Framework Introduction/Solutions/P15.RemoveTown/StartUp.cs
@@ -0,0 +1,49 @@
+﻿namespace SoftUni
+{
+    using System;
+    using System.Linq;
+    using System.Text;
+
+    using Data;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using (var context = new SoftUniContext())
+            {
+                var result = RemoveTown(context);
+                Console.WriteLine(result);
+            }
+        }
+
+        public static string RemoveTown(SoftUniContext context) 
+        {
+            var employees = context.Employees
+                .Where(e => e.Address.Town.Name == "Seattle")
+                .ToList();
+
+            foreach (var employee in employees)
+            {
+                employee.AddressId = null;
+            }
+
+            var addresses = context.Addresses
+                .Where(a => a.Town.Name == "Seattle")
+                .ToList();
+
+            context.Addresses.RemoveRange(addresses);
+
+            var townForRemove = context.Towns
+                .FirstOrDefault(t => t.Name == "Seattle");
+
+            context.Towns.Remove(townForRemove);
+
+            context.SaveChanges();
+
+            int deletedAddresses = addresses.Count();
+
+            return $"{deletedAddresses} addresses in Seattle were deleted";
+        }
+    }
+}
