9da9e5093ed84ced98711f74ea12c3d95a8d3048
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-10-11 11:44:52 +0300

    Add P04-5.Migrations

---
+++ Exercises/04_Code-First/Solutions/4-5.Migrations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _4_5.Migrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Exercises/04_Code-First/Solutions/P04-5.Migrations/Data/SalesContext.cs
@@ -0,0 +1,98 @@
+﻿namespace P03_SalesDatabase.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using Models;
+
+    public class SalesContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Sale> Sales { get; set; }
+
+        public DbSet<Store> Stores { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            ConfigureCustomerEntity(modelBuilder);
+
+            ConfigureProductEntity(modelBuilder);
+
+            ConfigureSaleEntity(modelBuilder);
+
+            ConfigureStoreEntity(modelBuilder);
+        }
+
+        private void ConfigureCustomerEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>()
+                .HasKey(c => c.CustomerId);
+
+            modelBuilder.Entity<Customer>()
+                .HasMany(s => s.Sales)
+                .WithOne(c => c.Customer);
+
+            modelBuilder.Entity<Customer>()
+                .Property(n => n.Name)
+                .HasMaxLength(100)
+                .IsUnicode(true);
+
+            modelBuilder.Entity<Customer>()
+                .Property(e => e.Email)
+                .HasMaxLength(80)
+                .IsUnicode(true);
+        }
+
+        private void ConfigureProductEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .HasKey(p => p.ProductId);
+
+            modelBuilder.Entity<Product>()
+                .HasMany(s => s.Sales)
+                .WithOne(p => p.Product);
+
+            modelBuilder.Entity<Product>()
+                .Property(n => n.Name)
+                .HasMaxLength(50)
+                .IsUnicode(true);
+
+            modelBuilder.Entity<Product>()
+                .Property(d => d.Description)
+                .HasMaxLength(250)
+                .HasDefaultValue("No description");
+        }
+
+        private void ConfigureSaleEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Sale>()
+                .HasKey(s => s.SaleId);
+
+            modelBuilder.Entity<Sale>()
+                .Property(d => d.Date)
+                .HasDefaultValueSql("GETDATE()");
+        }
+
+        private void ConfigureStoreEntity(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Store>()
+                .HasKey(s => s.StoreId);
+
+            modelBuilder.Entity<Store>()
+                .HasMany(s => s.Sales)
+                .WithOne(s => s.Store);
+
+            modelBuilder.Entity<Store>()
+                .Property(n => n.Name)
+                .HasMaxLength(80)
+                .IsUnicode(true);
+        }
+    }
+}
