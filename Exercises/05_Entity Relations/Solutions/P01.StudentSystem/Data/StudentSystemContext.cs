289116cd1d5f45b6827af531ac5b439e8bd1b327
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-10-11 19:15:06 +0300

    Add solutions

--- Exercises/05_Entity Relations/Solutions/P01.StudentSystem/Data/StudentSystemContext.cs
+++ Exercises/05_Entity Relations/Solutions/P01.StudentSystem/Data/StudentSystemContext.cs
@@ -2,8 +2,21 @@
 {
     using Microsoft.EntityFrameworkCore;
 
+    using Models;
+
     public class StudentSystemContext : DbContext
     {
+        DbSet<Student> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+
 
     }
 }
