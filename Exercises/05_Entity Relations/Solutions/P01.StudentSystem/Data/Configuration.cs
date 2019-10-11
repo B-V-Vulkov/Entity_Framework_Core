7b030628208204e745d8d09e25453226d118d3b6
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-10-11 17:42:05 +0300

    Delete folder

---
+++ Exercises/05_Entity Relations/Solutions/P01.StudentSystem/Data/Configuration.cs
@@ -0,0 +1,7 @@
+﻿namespace P01_StudentSystem.Data
+{
+    public abstract class Configuration
+    {
+        public const string ConnectionString = "Server=.;Database=StudentSystemDb;Integrated Security=True;";
+    }
+}
