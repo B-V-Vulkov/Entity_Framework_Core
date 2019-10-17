5ff35440b93d728c1dc17b677f753dd9440e3284
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-10-17 18:18:10 +0300

    Create Word.cs

---
+++ Demo Dictionary/DemoDictionary/DemoDictionary/Data/Models/Word.cs
@@ -0,0 +1,16 @@
+﻿namespace DemoDictionary.Data.Models
+{
+    using Enumerations;
+
+    public class Word
+    {
+        public int Id { get; }
+        public TypeOfWord Type { get; }
+
+        public Word(int id, TypeOfWord type)
+        {
+            Id = id;
+            Type = type;
+        }
+    }
+}
