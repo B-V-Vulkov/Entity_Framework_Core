037edea346c41363d013b22c2306fc023680e13b
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-11-21 10:33:49 +0200

    Add skeletons

---
+++ Exercises/07_JavaScript Object Notation - JSON/Solutions/01. Import Users_Product Shop/ProductShop/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace ProductShop
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using Newtonsoft.Json;
+    using ProductShop.Data;
+    using ProductShop.Models;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
