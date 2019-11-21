e2b7f8bad9fa8e77661108ceebe238fc83efa23a
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-11-21 14:58:18 +0200

    Add solutions

--- Exercises/07_JavaScript Object Notation - JSON/Solutions/01. Import Users_Product Shop/ProductShop/StartUp.cs
+++ Exercises/07_JavaScript Object Notation - JSON/Solutions/01. Import Users_Product Shop/ProductShop/StartUp.cs
@@ -1,10 +1,10 @@
 ﻿namespace ProductShop
 {
     using System;
-    using System.Collections.Generic;
     using System.IO;
     using System.Linq;
     using Newtonsoft.Json;
+
     using ProductShop.Data;
     using ProductShop.Models;
 
@@ -12,7 +12,102 @@
     {
         public static void Main()
         {
+            string fileName = "categories-products.json";
+            string directory = "Datasets";
+            string filePath = Path.Combine(directory, fileName);
 
+            using (var context = new ProductShopContext())
+            {
+                Console.WriteLine(GetSoldProducts(context));
+            }
+        }
+
+        //01.Import Users
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            var users = JsonConvert.DeserializeObject<User[]>(inputJson);
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Length}";
+        }
+
+        //02.Import Products
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            var products = JsonConvert.DeserializeObject<Product[]>(inputJson);
+
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            return $"Successfully imported {products.Length}";
+        }
+
+        //03.Import Categories
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+            var categories = JsonConvert.DeserializeObject<Category[]>(inputJson)
+                .Where(n => n.Name != null)
+                .ToArray();
+
+            context.Categories.AddRange(categories);
+            context.SaveChanges();
+
+            return $"Successfully imported {categories.Length}";
+        }
+
+        //04.Import Categories and Products
+        public static string ImportCategoryProducts(ProductShopContext context, string inputJson)
+        {
+            var categoriesProducts = JsonConvert.DeserializeObject<CategoryProduct[]>(inputJson);
+
+            context.CategoryProducts.AddRange(categoriesProducts);
+            context.SaveChanges();
+
+            return $"Successfully imported {categoriesProducts.Length}";
+        }
+
+        //05.Export Products In Range
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            var products = context.Products
+                .Where(p => p.Price >= 500 && p.Price <= 1000)
+                .Select(p => new
+                {
+                    name = p.Name,
+                    price = p.Price,
+                    seller = $"{p.Seller.FirstName} {p.Seller.LastName}",
+                })
+                .OrderBy(p => p.price)
+                .ToList();
+
+            return JsonConvert.SerializeObject(products, Formatting.Indented);
+        }
+
+        //06 Export Sold Products
+        public static string GetSoldProducts(ProductShopContext context)
+        {
+            var users = context.Users
+                .Where(u => u.ProductsSold.Count > 0)
+                .Select(u => new
+                {
+                    firstName = u.FirstName,
+                    lastName = u.LastName,
+                    soldProducts = u.ProductsSold.Select(sp => new
+                    {
+                        name = sp.Name,
+                        price = sp.Price,
+                        buyerFirstName = sp.Buyer.FirstName,
+                        buyerLastName = sp.Buyer.LastName,
+                    })
+                    .ToList()
+                })
+                .OrderBy(u => u.lastName)
+                .ThenBy(u => u.firstName)
+                .ToList();
+
+            return JsonConvert.SerializeObject(users, Formatting.Indented);
         }
     }
 }
