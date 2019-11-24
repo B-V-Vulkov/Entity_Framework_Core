13e0e08013ef6dba68a6d4906fd19963a8aad779
B-V-Vulkov <52923477+B-V-Vulkov@users.noreply.github.com>
2019-11-24 20:27:27 +0200

    Add Data Import

---
+++ Exams/01_Exam-01.09.2018/Solutions/VaporStore/DataProcessor/ImportDtos/ImportCardDto.cs
@@ -0,0 +1,18 @@
+﻿namespace VaporStore.DataProcessor.ImportDtos
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ImportCardDto
+    {
+        [Required]
+        [RegularExpression(@"^[0-9]{4}\s+[0-9]{4}\s+[0-9]{4}\s+[0-9]{4}$")]
+        public string Number { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[0-9]{3}$")]
+        public string Cvc { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+    }
+}
